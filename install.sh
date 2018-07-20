9202d24d265d824d1a435d20302017ff002d3dd9
Clark, Steve <steve@clarkhome.org>
2018-07-19 22:51:24 -0500

    Add powerlevel9k theme into custom configuration.

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ./uninstall.sh
 source ${DIR}/functions.sh
 install_files
+
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
+   git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
