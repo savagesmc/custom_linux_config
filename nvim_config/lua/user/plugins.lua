1a43e429155dc9660fc6f4428d39d7dde0b7aa95
Stephen M. Clark <stephen.m.clark@collins.com>
2024-04-05 22:19:30 -0500

    Add treesitter

--- nvim_config/lua/user/plugins.lua
+++ nvim_config/lua/user/plugins.lua
@@ -89,6 +89,12 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope.nvim"
   use "nvim-telescope/telescope-media-files.nvim"
 
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
   -- DirDiff
   use "vim-scripts/DirDiff.vim"
 
