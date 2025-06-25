58a38ff3cacfe23abf58334b06a5575d71fd5a2f
Stephen M. Clark <stephen.m.clark@collins.com>
2025-06-25 07:08:13 -0500

    Remove null-ls as it has been archived by the author.

--- nvim_config/lua/user/lsp/init.lua
+++ nvim_config/lua/user/lsp/init.lua
@@ -4,6 +4,34 @@ if not status_ok then
   return
 end
 
+local lspconfig = require('lspconfig')
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
@@ -14,4 +42,3 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 require'lspconfig'.clangd.setup{}
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
